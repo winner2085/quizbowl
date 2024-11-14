549b3033d19233bd0af0123b006ddb82d436d5fa
Erica <ericac2700@tuckahoeschools.org>
2024-11-14 13:30:11 +0000

    so close yet so far

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-const scoreContainer = document.querySelector('.score');
-const submitButton = document.querySelector('#submit-button');
-const input = document.querySelector('#input-field');
-const questions = document.getElementById('question');
+const submitButton = document.querySelector('.submit');
+const input = document.querySelector('.input-container');
+const questions = document.querySelector('.questions');
 const title = document.querySelector('.title');
-let answerList = ["Apollo", "10080", "New York", "Dove"];
+let scoreContainer = document.querySelector('.score-container');
 
+let score = 0;
 
-let questionList = ["Who was the Ancient Greek God of the Sun?",
-        "How many minutes are in a full week?",
-        "Which state was the first capital of the United States?",
-        "Which bird is a universal symbol of peace?"];
+let questionList = ['Who was the Ancient Greek God of the Sun?', 'How many minutes are in a full week?', 'Which state was the first capital of the United States?', 'Which bird is a universal symbol of peace?'];
 
-questions.textContent = questionList[0];
-input.textContent = answerList[0];
+let answerList = ['Apollo', '10080', 'New York', 'Dove'];
 
+let questionIndex = 0;
+questions.textContent = questionList[questionIndex];
 
-if (input === 0){
+let submitFunction = () =>{
         submitButton.addEventListener('click', () =>{
-                
+                if(input.value === answerList[questionIndex]){
+                        questionIndex +- 1;
+                        score +- 1;
+                        questionIndex.textContent = questionList[questionIndex];
+                        scoreContainer.textContent = "Score: " + score;
+                }else{
+                        score -= 1;
+                        scoreContainer.textContent = "Score: " + score;
+                }
+        // Put endgame function here
+        endGame ();
         });
 }
+
+let endGame = () =>{
+        if (questionIndex > 3){
+                scoreContainer.textContent = "Final Score: " + score;
+                question.textContent = "Great Job, you know your stuff! Play again.";
+                input.style.display = "none";
+                submitButton.style.display = "none";
+        }
+}
+
+submitFunction ();
+
+
