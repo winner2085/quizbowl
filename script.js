652907549cef87bd0344f474306f20e0fa39b9ba
Erica <ericac2700@tuckahoeschools.org>
2024-11-18 04:30:21 +0000

    I fixed itgit add .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const submitButton = document.querySelector('.submit');
-const input = document.querySelector('.input-container');
+const input = document.getElementById('input-container');
 const questions = document.querySelector('.questions');
 const title = document.querySelector('.title');
 let scoreContainer = document.querySelector('.score-container');
@@ -8,15 +8,16 @@ let score = 0;
 
 let questionList = ['Who was the Ancient Greek God of the Sun?', 'How many minutes are in a full week?', 'Which state was the first capital of the United States?', 'Which bird is a universal symbol of peace?'];
 
-let answerList = ['Apollo', '10080', 'New York', 'Dove'];
+let answerList = ['Apollo', '10080', 'York, PA', 'Dove'];
 
 let questionIndex = 0;
 questions.textContent = questionList[questionIndex];
 
+console.log(input.value);
 let submitFunction = () =>{
         submitButton.addEventListener('click', () =>{
                 if(input.value === answerList[questionIndex]){
-                        questionIndex +- 1;
+                        questionIndex += 1;
                         score += 1;
                         questions.textContent = questionList[questionIndex];
                         scoreContainer.textContent = "Score: " + score;
